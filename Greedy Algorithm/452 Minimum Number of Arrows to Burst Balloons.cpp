8b3a07523c7a9bb03952a90579fd8b3c87d953c6
DaXiaoSHI <readsjj@163.com>
2021-04-03 18:26:49 +0800

    leetcode452 added

---
+++ Greedy Algorithm/452 Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,19 @@
+#include <Leetcode/allInclude.hpp>
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        int size=points.size();
+        if(points.empty()) return 0;
+        sort(points.begin(), points.end(), [](vector<int>&a, vector<int>&b){return a[1]<b[1];});
+
+        int cnt=1, prev=points[0][1];
+        for(int i=1; i<size; ++i){
+            if(points[i][0]>prev){
+                ++cnt;
+                prev=points[i][1];
+            }
+        }
+        return cnt;
+    }
+};
