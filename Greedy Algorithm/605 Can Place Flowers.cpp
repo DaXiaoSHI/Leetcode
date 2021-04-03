5f96355a215537a1bcf3fb7f0ae8a1be3cf9e4f5
DaXiaoSHI <readsjj@163.com>
2021-04-03 17:21:35 +0800

    Leetcode 605 added

---
+++ Greedy Algorithm/605 Can Place Flowers.cpp
@@ -0,0 +1,27 @@
+#include <Leetcode/allInclude.hpp>
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int size=flowerbed.size();
+        int cnt=0, prev=-1;
+        for(int i=0; i<size; ++i) {
+            if(flowerbed[i]==1){
+                if(prev<0){
+                    cnt+=i/2;
+                }else{
+                    cnt+=(i-prev-2)/2;
+                }
+                if(cnt>=n)
+                    return true;
+                prev=i;
+            }
+        }
+        if(prev<0)
+            cnt+=(size+1)/2;
+        else
+            cnt+=(size-prev-1)/2;
+        
+        return cnt>=n;
+    }
+};
